faf2c127bb2c6ac63976e573e95c74b03528c9b7
vadorofeeva <makeup-style@mail.ru>
2024-08-01 17:57:02 +0300

    All methods and connection with DB done, but it doesn't work

--- server/src/main/java/ru/otus/ohmyval/homeworks/chat/server/AuthenticationService.java
+++ server/src/main/java/ru/otus/ohmyval/homeworks/chat/server/AuthenticationService.java
@@ -2,6 +2,8 @@ package ru.otus.ohmyval.homeworks.chat.server;
 
 public interface AuthenticationService {
     String getNicknameByLoginAndPassword(String login, String password);
+
+//    boolean register(String login, String password, String nickname, Role role);
     boolean register(String login, String password, String nickname);
     boolean isLoginAlreadyExist(String login);
     boolean isNicknameAlreadyExist(String nickname);
--- server/src/main/java/ru/otus/ohmyval/homeworks/chat/server/DatabaseAuthenticationService.java
+++ server/src/main/java/ru/otus/ohmyval/homeworks/chat/server/DatabaseAuthenticationService.java
@@ -3,7 +3,6 @@ package ru.otus.ohmyval.homeworks.chat.server;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class DatabaseAuthenticationService implements AuthenticationService {
     private class User {
@@ -42,7 +41,6 @@ public class DatabaseAuthenticationService implements AuthenticationService {
             this.login = login;
             this.password = password;
             this.nickname = nickname;
-//            this.role = role;
         }
     }
 
@@ -66,103 +64,78 @@ public class DatabaseAuthenticationService implements AuthenticationService {
 
 
     private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/chat";
-    private static final String USERS_ADD_QUERY = "INSERT * INTO users (id, login, password, nickname) values (?, 'login' + ?, 'pass' + ?, 'nick' + ?)";
+    private static final String USERS_ADD_QUERY = "INSERT * INTO users (login, password, nickname) values ('login' + ?, 'pass' + ?, 'nick' + ?)";
     private static final String USER_REGISTER_QUERY = "INSERT * INTO users (login, password, nickname) values (?, ?, ?)";
-    private static final String USERS_QUERY = "SELECT * FROM users"; // делаем запрос в БД о пользователях (из табл 1)
     private static final String USER_ROLE_ADMIN_QUERY = "INSERT * INTO user_role (user_id, role_id) values (?, '1')";
     private static final String USER_ROLE_USER_QUERY = "INSERT * INTO user_role (user_id, role_id) values (?, '2')";
     private static final String USER_ROLE_QUERY = "select r.id as id, r.title as title from user_role ur left join roles r ON r.id=ur.role_id where ur.user_id = ?";
 
     private List<User> users;
 
+    public DatabaseAuthenticationService() {
+        this.users = new ArrayList<>();
+        databaseOperation();
+    }
+
     public void databaseOperation() {
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, "postgres", "")) {
-            try (PreparedStatement ps = connection.prepareStatement(USERS_ADD_QUERY)) {
-                for (int i = 1; i <= 10; i++) {
-                    ps.setInt(1, i);
-                    try (ResultSet usersResultSet = ps.executeQuery()) {
-                        while (usersResultSet.next()) {
-                            int id = usersResultSet.getInt("id");
-                            String login = usersResultSet.getString("login");
-                            String password = usersResultSet.getString("password");
-                            String nickname = usersResultSet.getString("nickname");
-                            User user = new User(id, login, password, nickname);
-                            users.add(user);
-                        }
-                    }
-                }
-            }
-            try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_ADMIN_QUERY)) {
-                for (int i = 1; i <= 2; i++) {
-                    ps.setInt(1, i);
-                }
-            }
-            try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_USER_QUERY)) {
-                for (int i = 3; i <= 10; i++) {
-                    ps.setInt(1, i);
-                }
-            }
-            try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_QUERY)) {
-                for (User user : users) {
-                    ps.setInt(1, user.getId());
-                    try (ResultSet usersResultSet = ps.executeQuery()) {
-                        while (usersResultSet.next()) {
-                            int id = usersResultSet.getInt("id");
-                            String title = usersResultSet.getString("title");
-                            user.setRole(new Role(id, title));
-                        }
-                    }
-                }
-            }
+            fillUsers(connection);
+            setRoleAdmin(connection);
+            setRoleUser(connection);
+            extractUserRoles(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
-
-//        try(Statement statement = connection.createStatement() ) { // создали объект, класса Statement
-//            try(ResultSet usersResultSet = statement.executeQuery(USERS_QUERY)) { // выполняем запрос - подаем его в аргументы,
-//                // записываем в переменную usersResultSet
-//                while(usersResultSet.next()) { //используем итератор; пока получаем следующий элемент, то записываем его
-//                    int id = usersResultSet.getInt("id"); // берем значение из первой колонки нашей таблицы
-//                    // (колонки нумеруются с единицы); можно указать как название колонки, так и ее номер
-//                    String email = usersResultSet.getString(2); // используем обычный String
-//                    String password = usersResultSet.getString(3);
-//                    User user = new User(id, password, email); // создали нового юзера с полями
-//                    users.add(user); // добавили его в лист
-//                }
-//
-//            }
-//        }
-//        // продолжаем работать внутри созданного подключения к БД Connection connection
-//        try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_QUERY)) { // создаем объект PreparedStatement ps,
-//            // подаем наш запрос в аргументы
-//            for (User user : users) { // проходимся по списку пользователей, для каждого из них создаем PreparedStatement
-//                List<Role> roles = new ArrayList<>(); // создали список ролей (он новый для каждого пользователя)
-//                ps.setInt(1, user.getId()); // у нашего запроса ps устанавливаем вместо ? параметр 1
-//                // (т.е. это первый человек из таблицы user_to_role), и запрашиваю id этого человека
-//                try(ResultSet usersResultSet = ps.executeQuery()) { // выполняем запрос, он без аргументов, записали в usersResultSet
-//                    while(usersResultSet.next()) { //используем итератор
-//                        int id = usersResultSet.getInt("id"); //нас интересует колонка 3 и 4 (они относятся только к табличке Roles)
-//                        String name = usersResultSet.getString("name");
-//                        Role role = new Role(id, name); // создали новый объект класса Role, с id и name
-//                        roles.add(role); // добавили в список ролей новый объект
-//                    }
-//                    user.setRoles(roles); // подаем пользователю его список ролей (у каждого пользователя он свой)
-//                }
-//            }
-//        }
-
-
-//    public DatabaseAuthenticationService() {
-//        this.users = new ArrayList<>();
-//        for (int i = 1; i <= 10; i++) {
-//            this.users.add(new User("login" + i, "pass" + i, "nick" + i, Role.USER));
-//        }
-//        users.get(0).setRole(Role.ADMIN);
-//        users.get(1).setRole(Role.ADMIN);
-//
-//    }
+    private void fillUsers(Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(USERS_ADD_QUERY)) {
+            for (int i = 1; i <= 10; i++) {
+                ps.setInt(1, i);
+                ps.setInt(2, i);
+                ps.setInt(3, i);
+                try (ResultSet usersResultSet = ps.executeQuery()) {
+                    while (usersResultSet.next()) {
+                        int id = usersResultSet.getInt("id");
+                        String login = usersResultSet.getString("login");
+                        String password = usersResultSet.getString("password");
+                        String nickname = usersResultSet.getString("nickname");
+                        User user = new User(id, login, password, nickname);
+                        users.add(user);
+                    }
+                }
+            }
+        }
+    }
+    private static void setRoleAdmin(Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_ADMIN_QUERY)) {
+            for (int i = 1; i <= 2; i++) {
+                ps.setInt(1, i);
+                ps.execute();
+            }
+        }
+    }
+    private static void setRoleUser(Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_USER_QUERY)) {
+            for (int i = 3; i <= 10; i++) {
+                ps.setInt(1, i);
+                ps.execute();
+            }
+        }
+    }
+    private void extractUserRoles(Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(USER_ROLE_QUERY)) {
+            for (User user : users) {
+                ps.setInt(1, user.getId());
+                try (ResultSet usersResultSet = ps.executeQuery()) {
+                    while (usersResultSet.next()) {
+                        int id = usersResultSet.getInt("id");
+                        String title = usersResultSet.getString("title");
+                        user.setRole(new Role(id, title));
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     public String getNicknameByLoginAndPassword(String login, String password) {
@@ -174,17 +147,6 @@ public class DatabaseAuthenticationService implements AuthenticationService {
         return null;
     }
 
-//    @Override
-//    public boolean register(String login, String password, String nickname, Role role) {
-//        if (isLoginAlreadyExist(login)) {
-//            return false;
-//        }
-//        if (isNicknameAlreadyExist(nickname)) {
-//            return false;
-//        }
-//        users.add(new DatabaseAuthenticationService.User(login, password, nickname, role));
-//        return true;
-//    }
     @Override
     public boolean register(String login, String password, String nickname) {
         if (isLoginAlreadyExist(login)) {
@@ -194,26 +156,41 @@ public class DatabaseAuthenticationService implements AuthenticationService {
             return false;
         }
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, "postgres", "")) {
-            try (PreparedStatement ps = connection.prepareStatement(USER_REGISTER_QUERY)) {
-                ps.setString(1, login);
-                ps.setString(2, password);
-                ps.setString(3, nickname);
-                try (ResultSet usersResultSet = ps.executeQuery()) {
-                    int id = usersResultSet.getInt("id");
-                    String addedLogin = usersResultSet.getString("login");
-                    String addedPassword = usersResultSet.getString("password");
-                    String addedNickname = usersResultSet.getString("nickname");
-                    User user = new User(id, addedLogin, addedPassword, addedNickname);
-                    users.add(user);
-                }
-
-            }
+            addNewUser(login, password, nickname, connection);
 
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
         return true;
+    }
+
+    private void addNewUser(String login, String password, String nickname, Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(USER_REGISTER_QUERY)) {
+            ps.setString(1, login);
+            ps.setString(2, password);
+            ps.setString(3, nickname);
+            try (ResultSet usersResultSet = ps.executeQuery()) {
+                int id = usersResultSet.getInt("id");
+                String addedLogin = usersResultSet.getString("login");
+                String addedPassword = usersResultSet.getString("password");
+                String addedNickname = usersResultSet.getString("nickname");
+                User user = new User(id, addedLogin, addedPassword, addedNickname);
+                users.add(user);
+                try (PreparedStatement preparedStatement = connection.prepareStatement(USER_ROLE_USER_QUERY)){
+                    preparedStatement.setInt(1, user.getId());
+                    preparedStatement.execute();
+                }
+                try (PreparedStatement pStatement = connection.prepareStatement(USER_ROLE_QUERY)) {
+                    pStatement.setInt(1, user.getId());
+                        try (ResultSet newResultSet = pStatement.executeQuery()) {
+                                int roleId = newResultSet.getInt("id");
+                                String title = newResultSet.getString("title");
+                                user.setRole(new Role(roleId, title));
+                    }
+                }
+            }
+        }
     }
 
     @Override
